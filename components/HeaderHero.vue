1e6f8f298655d2353c37ca5e526505f8612d3763
Robin Hamill <hamillrobin@gmail.com>
2018-06-10 09:59:35 -0400

    Add HeaderHero comp

---
+++ components/HeaderHero.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="header-hero">
+    <h2>Hi, I'm Robin.</h2>
+    <p>I'm an independent Shopify and web application developer in Toronto. I build intuitive and functional web apps and e-commerce stores that engage users and solve problems.</p>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'HeaderHero'
+  }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
