e431091f848cb62b14d44235213bd876ed50fcc6
Robin Hamill <hamillrobin@gmail.com>
2018-06-16 21:45:02 -0400

    Create SubmitButton comp

---
+++ components/SubmitButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <input type="submit" :value="value">
+</template>
+
+<script>
+export default {
+  name: "SubmitButton",
+  props: ["value"],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+@import '../assets/styles/_vars'
+@import '../assets/styles/_mixins'
+
+input[type=submit]
+  text-decoration none
+  padding 1.5em 4em
+  border-radius 50px
+  border none
+  background $turquoise
+  color $white
+  width 470px
+</style>
