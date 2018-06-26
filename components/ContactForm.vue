c968be6244625e3a9945aa4f5ff175bd5cdceaf8
Robin Hamill <hamillrobin@gmail.com>
2018-06-26 18:36:40 -0400

    Reorgf and style form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -29,31 +29,27 @@
         </div>
       </section>
       
-      <h3>Services Needed</h3>
       <section class="services">
+        <h3>Services Needed</h3>
         
-        <input type="radio" name="Service" id="ecommerce" value="ecommerce">
-        <label for="ecommerce">e-commerce</label>
-        <input type="radio" name="Service" id="webApp" value="webApp">
-        <label for="webApp">Web App</label>
-        <input type="radio" name="Service" id="shopifyApp" value="shopifyApp">
-        <label for="shopifyApp">Shopify App</label>
-        <input type="radio" name="Service" id="static" value="static">
-        <label for="static">Static Webpage</label>
+        <service-toggle
+          v-for="service in services"
+          :service="service"
+          :key="service.id"
+        />
         
-        <label for="other">Other: </label>
         <input type="text" id="other" name="Service--other">
       </section>
 
-      <h3>Timeline</h3>
       <section class="timeline">
+        <h3>Timeline</h3>
 
         <label class="visuallyhidden" for="timeline">Timeline</label>
         <input type="range" name="timeline" id="timeline">
       </section>
 
-      <h3>Budget</h3>
       <section class="budget">
+        <h3>Budget</h3>
 
         <label class="visuallyhidden" for="budget">Budget</label>
         <input type="range" name="budget" id="budget">
@@ -67,11 +63,49 @@
 
 <script>
 import SubmitButton from "~/components/SubmitButton";
+import ServiceToggle from "~/components/ServiceToggle";
 
 export default {
   name: "ContactForm",
   components: {
-    SubmitButton
+    SubmitButton,
+    ServiceToggle
+  },
+  data() {
+    return {
+      services: [
+        {
+          name: "Service",
+          title: "e-commerce",
+          id: "ecommerce",
+          image: "eCommerce.png"
+        },
+        {
+          name: "Service",
+          title: "Web App",
+          id: "webApp",
+          image: "webApp.png"
+        },
+        {
+          name: "Service",
+          title: "Shopify App",
+          id: "shopifyApp",
+          image: "shopify.png"
+        },
+        {
+          name: "Service",
+          title: "Static Webpage",
+          id: "static",
+          image: "staticWebpage.png"
+        },
+        {
+          name: "Service",
+          title: "Other",
+          id: "other",
+          image: "other.png"
+        }
+      ]
+    };
   }
 };
 </script>
@@ -84,7 +118,7 @@ form
   display grid
   grid-template-columns 1fr 1fr
   grid-auto-rows auto
-  grid-gap 40px
+  grid-gap 4rem
 
   h3
     color $darkblue
@@ -94,23 +128,23 @@ form
 
   label
     position absolute
-    top 10px
-    left 10px
+    top 1rem
+    left 1rem
     line-height 1
     font-size 1.2rem
-    color #D2D2D2
 
   input, textarea
+    background $white
     width 100%
     border 1px solid $darkblue
-    border-radius 5px
-    padding 10px
+    border-radius 0.5rem
+    padding 1.5rem 1rem
 
 .about
   grid-column 1 / -1
   display grid
-  grid-template-columns 1fr 1fr 80px 1fr 1fr
-  grid-gap 15px 10px
+  grid-template-columns 1fr 1fr 8rem 1fr 1fr
+  grid-gap 1.5rem 1rem
 
 .form-group--first-name
   grid-column 1 / span 1
@@ -131,4 +165,24 @@ textarea
   min-height 185px
   max-width 100%
   resize none
+
+.services
+  grid-column 1 / -1
+  display grid
+  grid-template-columns repeat(5, 1fr) 2fr
+  align-items flex-end
+  grid-gap 0 28px
+
+  h3
+    grid-column 1 / -1
+
+.timeline
+  grid-column 1 / span 1
+
+.budget
+  grid-column -1 / span -1
+
+.timeline, .budget
+  input[type='range']
+    width 100%
 </style>
