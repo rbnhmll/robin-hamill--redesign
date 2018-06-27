da0ac4fed2e3209de6311098bd7736cafd7f29e0
Robin Hamill <hamillrobin@gmail.com>
2018-06-26 23:16:07 -0400

    Add comps

---
+++ components/FeaturedProjects.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <h2 class="h1">Featured Projects</h2>
+    <project-loop :projects="projects" />
+    <see-more-button url="/projects" />
+  </section>
+</template>
+
+<script>
+import ProjectLoop from "~/components/ProjectLoop.vue";
+import SeeMoreButton from "~/components/SeeMoreButton.vue";
+
+export default {
+  name: "FeaturedProjects",
+  props: ["projects"],
+  components: {
+    ProjectLoop,
+    SeeMoreButton
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+@import '../assets/styles/_vars'
+@import '../assets/styles/_mixins'
+
+h2
+  text-align center
+</style>
---
+++ components/TechIcons.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul>
+    <li v-for="icon in icons" :key="icon.name">
+      <font-awesome-icon :icon="icon.iconKey" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "TechIcons",
+  data() {
+    return {
+      icons: [
+        {
+          name: "Shopify",
+          iconKey: ["fab", "twitter"]
+        },
+        {
+          name: "Shopify Plus",
+          iconKey: ["fab", "github"]
+        },
+        {
+          name: "Vue",
+          iconKey: ["fab", "codepen"]
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+@import '../assets/styles/_vars'
+@import '../assets/styles/_mixins'
+
+ul
+  display flex
+  justify-content center
+  list-style none
+  margin 0
+  padding 0
+</style>
