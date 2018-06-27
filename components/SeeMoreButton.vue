cb76f79779875ae18492189556cd00bc7b0cd4d2
Robin Hamill <hamillrobin@gmail.com>
2018-06-26 23:14:50 -0400

    Create See More Comp

---
+++ components/SeeMoreButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <nuxt-link :to="url">See More Projects</nuxt-link>
+</template>
+
+<script>
+export default {
+  name: "SeeMoreButton",
+  props: ["url", "value"],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+@import '../assets/styles/_vars'
+@import '../assets/styles/_mixins'
+
+a
+  text-decoration none
+  display block
+  text-align center
+  padding 1rem
+  border-radius 50px
+  background $turquoise
+  color $white
+  width 340px
+  font-size 2.4rem
+  font-weight 600
+  margin 11rem auto
+</style>
