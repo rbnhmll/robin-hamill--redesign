16e3ce18c13ae7d101051560e6b70d2946a0c473
Robin Hamill <hamillrobin@gmail.com>
2018-08-06 21:36:43 -0400

    Update header message

--- components/HeaderHero.vue
+++ components/HeaderHero.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="header-hero wrapper--medium">
     <h2 class="header-hero__title h1">Hi, I'm Robin.</h2>
-    <p>I'm an independent Shopify and web application developer in Toronto. I build intuitive and functional web apps and e-commerce stores that engage users and solve problems.</p>
+    <p>I'm an independent Shopify and web application developer in Toronto. I help online merchants create unique buying experience that drive sales.</p>
   </section>
 </template>
 
