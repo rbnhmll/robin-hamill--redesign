114c07cd7b0adcb7eb8470e00a14fc9185a93304
Robin Hamill <hamillrobin@gmail.com>
2018-12-26 15:25:52 -0700

    Update contentful

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,8 +1,8 @@
 const contentful = require('contentful')
 
 const client = contentful.createClient({
-  space: process.env.CTF_SPACE_ID,
-  accessToken: process.env.CTF_CDA_ACCESS_TOKEN
+  accessToken: process.env.CTF_CDA_ACCESS_TOKEN,
+  space: process.env.CTF_SPACE_ID
 })
 
 module.exports = client
