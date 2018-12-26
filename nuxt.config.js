2623aa0a23659249661877495487c7753579dc7a
Robin Hamill <hamillrobin@gmail.com>
2018-12-26 15:27:57 -0700

    Update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -228,7 +228,7 @@ module.exports = {
     }
   },
   env: {
-    space: process.env.CTF_SPACE_ID,
-    accessToken: process.env.CTF_CDA_ACCESS_TOKEN
+    space: process.env.CTF_SPACE_ID || '',
+    accessToken: process.env.CTF_CDA_ACCESS_TOKEN || ''
   }
 }
